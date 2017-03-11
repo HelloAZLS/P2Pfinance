821dd4532082532de4f548096617a85dad59233c
hua <huad@qq.com>
2017-03-11 16:19:17 +0800

    //将要导入ButterkNife

---
+++ app/src/main/java/ysg/gdcp/cn/p2pfinance/fregment/TouZiFragment.java
@@ -0,0 +1,23 @@
+package ysg.gdcp.cn.p2pfinance.fregment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ysg.gdcp.cn.p2pfinance.R;
+
+/**
+ * Created by Administrator on 2017/3/11.
+ */
+
+public class TouZiFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = View.inflate(container.getContext(), R.layout.fragment_tuozi,null);
+        return  view;
+    }
+}
