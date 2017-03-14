198f3f5ad5b03afa6461894e605d50b1236bdfb9
hua <huad@qq.com>
2017-03-14 19:13:05 +0800

    做到投资列表

--- app/src/main/java/ysg/gdcp/cn/p2pfinance/fregment/TouZiFragment.java
+++ app/src/main/java/ysg/gdcp/cn/p2pfinance/fregment/TouZiFragment.java
@@ -1,12 +1,20 @@
 package ysg.gdcp.cn.p2pfinance.fregment;
 
-import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.loopj.android.http.RequestParams;
+import com.viewpagerindicator.TabPageIndicator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.Bind;
 import ysg.gdcp.cn.p2pfinance.R;
 import ysg.gdcp.cn.p2pfinance.utils.UIutils;
 
@@ -14,11 +22,78 @@ import ysg.gdcp.cn.p2pfinance.utils.UIutils;
  * Created by Administrator on 2017/3/11.
  */
 
-public class TouZiFragment extends Fragment {
-    @Nullable
+public class TouZiFragment extends BaseFragment {
+
+    @Bind(R.id.title_left)
+    ImageView titleLeft;
+    @Bind(R.id.title_tv)
+    TextView titleTv;
+    @Bind(R.id.title_right)
+    ImageView titleRight;
+    @Bind(R.id.tab_indictor)
+    TabPageIndicator tabIndictor;
+    @Bind(R.id.pager)
+    ViewPager pager;
+
+    private List<Fragment> fragmentList = new ArrayList<>();
+
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view =UIutils.getXMLView(R.layout.fragment_tuozi);
-        return  view;
+    protected void initTitle() {
+        titleTv.setText("我要投资");
+        titleLeft.setVisibility(View.INVISIBLE);
+        titleRight.setVisibility(View.INVISIBLE);
+    }
+
+    @Override
+    protected void initData(String content) {
+        initFragment();
+        pager.setAdapter(new MyAdapter(getFragmentManager()));
+
+        tabIndictor.setViewPager(pager);
+    }
+
+    private void initFragment() {
+        ProductListFragmemt productListFragmemt = new ProductListFragmemt();
+        ProductReCommendFragmemt productReCommendFragmemt = new ProductReCommendFragmemt();
+        ProductHotFragmemt productHotFragmemt = new ProductHotFragmemt();
+        fragmentList.add(productListFragmemt);
+        fragmentList.add(productReCommendFragmemt);
+        fragmentList.add(productHotFragmemt);
+    }
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.fragment_touzi;
+    }
+
+    @Override
+    protected RequestParams getParams() {
+        return new RequestParams();
+    }
+
+    @Override
+    protected String getUrl() {
+        return "";
+    }
+
+    private class MyAdapter extends FragmentPagerAdapter {
+        public MyAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return fragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragmentList == null ? 0 : fragmentList.size();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return UIutils.getStringArr(R.array.touzi_tab)[position];
+        }
     }
 }
